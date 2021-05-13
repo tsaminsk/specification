a25d088e2da98fe8e03797e181a53b5254fc4618
Talai <tsa@tut.by>
2021-05-13 12:53:29 +0300

    feat: add modal win & some edits

--- source/js/main.js
+++ source/js/main.js
@@ -15,23 +15,4 @@ $(document).ready(function(){
 			$('body').css('overflow', 'auto');
 		}
 	});
-
-	// $('input[type=tel]')
-	// 	.inputmask("8 (999) 999 99 99");
-
-
-	// $('.js-popup-img')
-	// 	.magnificPopup({
-	// 		type:'image',
-	// 		closeOnContentClick: true,
-	// 		fixedContentPos: true,
-	// 		mainClass: 'mfp-no-margins mfp-with-zoom',
-	// 		image: {
-	// 			verticalFit: true
-	// 		},
-	// 		zoom: {
-	// 			enabled: true,
-	// 			duration: 300
-	// 		}
-	// 	});
 });
